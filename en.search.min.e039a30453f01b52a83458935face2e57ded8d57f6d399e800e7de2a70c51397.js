0f81526bd4f5cfd5079d45eba686d334ffc8376e
kraashen <tomi.juntunen@iki.fi>
2022-09-11 16:22:34 +0000

    deploy: a50ed7a9723c750bccb23d524b0c485aaf0e2aba

--- en.search.min.e039a30453f01b52a83458935face2e57ded8d57f6d399e800e7de2a70c51397.js
+++ en.search.min.e039a30453f01b52a83458935face2e57ded8d57f6d399e800e7de2a70c51397.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.8408e24704f155d1e47db906339ede58ad401228ea13b7b40bdc345040749c7f.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.b321f2c01f899a3de44fd7de1fe4142265a9b35bcf44c5d9eafd38442b00a587.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
